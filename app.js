9c98f8e6761b8e654369618a6b2b9dfd9867a0db
G <gauravgol34@gmail.com>
2026-02-03 22:00:12 +0530

    stop rabbit mq

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const app = express();
 
 
 // Enable CORS
-// app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  }));
+app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+//   }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
